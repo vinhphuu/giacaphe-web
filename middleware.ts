9255a5839deef0f668de149060b7a3308ed7df23
Phú <vinhphutvd@gmail.com>
2026-03-10 20:59:31 +0700

    fix: delete middleware.ts completely - causes _middleware in all route outputs

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function middleware(request: NextRequest) {
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: [],
-};
